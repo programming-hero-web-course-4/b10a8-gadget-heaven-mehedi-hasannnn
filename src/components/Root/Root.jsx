d54021668139386a300162b1eb00af716a2cda58
mehedi-hasannnn <mhasan2999@gmail.com>
2025-01-12 10:49:21 +0600

    initial commit with router tailwind and daisyui setup done

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2 className='text-4xl text-red-500'>This is Homepage</h2>
+        </div>
+    );
+};
+
+export default Root;
